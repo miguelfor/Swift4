eeac45a872c8eba22b049a3976d70da5f1d01ef6
miguel.forero <miguelfor@hotmail.com>
2018-02-12 10:20:25 -0800

    Add Strings

---
+++ Strings.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let msg = "welcome ombe"
+var name = "Alejo Duran"
+let fullMsg = msg + "," + name
+print(fullMsg)
+let fullMsg2="\(name),\(msg)"
+print(fullMsg2)
+print("Welcome \(name), in my calss")
+name.append(msg)
+msg.capitalized
+msg.lowercased()
+msg.uppercased()
+
+let info = " he works for software departament"
+//if contains the word work return true
+info.contains("works")
+info.contains("hussein")
+let msg3 = "hi"
+msg3.isEmpty
+
+let longMsg = """
+hi hussein how are you doing
+how is your works for know
+"""
+
+
+
+
+
