37152635c0401bd8ac05c3f0d7a7dab8616cdf41
miguel.forero <miguelfor@hotmail.com>
2018-02-12 09:33:10 -0800

    Add find my age

---
+++ FindMyAge.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//input
+
+let yearOfBirth = 2000
+
+
+
+//process
+
+let myAge = 2017 - yearOfBirth
+
+//output
+
+print(myAge)
