84b447f8b5e6faa0f76ba3ee10f73e00ab8c34bf
miguel.forero <miguelfor@hotmail.com>
2018-02-12 09:32:33 -0800

    First commit ConvertDataType

---
+++ ConvertDataType.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var strNumber:String = "123456"
+var doubleNumber:Double = 1.2
+var intNumberIn = 123
+
+// conert str to int
+var intNumber1:Int? = Int(strNumber)
+var intNumber2 = Int(strNumber)
+
+// convert double to int
+var intDouble = Int(doubleNumber)
+
+// conver int to string
+var intString = 343434
+var strString1:String? = String(intString)
+var strString2 = String(intString)
+
+// convert double to string
+var strdouble = String(doubleNumber)
+
+// convert to double
+var doubleNumber1 = Double(strNumber)
+var doubleInt = Double(intNumberIn)
+
+// convert to boolean
+var strBoolean = "true"
+var bolStr = Bool(strBoolean)
+
+
+
+
+
+
