b081ef7587a3aa68815ece668b85f4ea338bb959
miguel.forero <miguelfor@hotmail.com>
2018-02-12 10:20:48 -0800

    Add example var Vs val

---
+++ varVsval.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+/* this app compare using val and let
+ first part val
+ and second part let
+ */
+
+import UIKit
+
+var str = "Hello, playground"
+
+// how to use Val
+var name = "hussein"
+print (name)
+name = "jena"
+print(name)
+
+// how to use let
+let CreditNumber = "8455125748"
+print(CreditNumber)
+//CreditNumber = "545453"
+print(CreditNumber)
+
