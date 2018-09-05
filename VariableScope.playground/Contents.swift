9c9d508e82b97e6cb71f04215e4d63910439fd7d
miguel.forero <miguelfor@hotmail.com>
2018-09-05 08:44:35 -0700

    Add VariableScope

---
+++ VariableScope.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let name = "miguel"
+func show(){
+    print(name)
+}
+
+print(name)
+
+
+let age = 18
+if age > 17 {
+    let job = "developer"
+    print(job)
+    show()
+}
+//>><<
